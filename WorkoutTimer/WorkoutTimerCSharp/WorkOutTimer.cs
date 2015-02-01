c5bc2d629804d4436e4b24c20f28f1317bc12a27
pepinho24 <p_mil4ev@hotmail.com>
2015-02-01 17:52:44 +0200

    Implemented music player

--- WorkoutTimer/WorkoutTimerCSharp/WorkOutTimer.cs
+++ WorkoutTimer/WorkoutTimerCSharp/WorkOutTimer.cs
@@ -7,18 +7,18 @@
     {
         public static void Main()
         {
-           // RunTimer();
-            var player = new MusicPlayer();
+            RunTimer();
+            //var player = new MusicPlayer();
 
-            player.open(@"E:/MyMusicFile.mp3");
-            Console.WriteLine("Playing file!");
-            Thread.Sleep(20000);
-            player.pause();
-            Console.WriteLine("Paused!");
-            Thread.Sleep(2000);
-            player.play();
-            Console.WriteLine("Playing again!");
-            Thread.Sleep(20000);
+            //player.open(@"E:/MyMusicFile.mp3");
+            //Console.WriteLine("Playing file!");
+            //Thread.Sleep(20000);
+            //player.pause();
+            //Console.WriteLine("Paused!");
+            //Thread.Sleep(2000);
+            //player.play();
+            //Console.WriteLine("Playing again!");
+            //Thread.Sleep(20000);
         }
 
         private static void GetUserSettings(out int numOfReps, out int numOfExercises, out int numOfSeries, out int restTimeBetweenSeries, out int restTimeBetweenExercises, out int repTime)
@@ -58,15 +58,32 @@
 
             GetUserSettings(out numOfReps, out numOfExercises, out numOfSeries, out restTimeBetweenSeries, out restTimeBetweenExercises, out repTime);
 
-            Console.WriteLine("Starting the timer...");
+            var player = new MusicPlayer();
 
+            //player.open(@"E:/MyMusicFile.mp3");
+            //Console.WriteLine("Playing file!");
+            //Thread.Sleep(20000);
+            //player.pause();
+            //Console.WriteLine("Paused!");
+            //Thread.Sleep(2000);
+            //player.play();
+            //Console.WriteLine("Playing again!");
+            //Thread.Sleep(20000);
+
+            Console.WriteLine("Starting the timer...");
+            Console.WriteLine("Playing file!");
+            
+            player.open(@"E:/MyMusicFile.mp3");
+            
             for (int exerciseNumber = 0; exerciseNumber < numOfExercises; exerciseNumber++)
             {
                 Console.WriteLine("Exercise number {0}", exerciseNumber + 1);
 
+
                 for (int serieNumber = 0; serieNumber < numOfSeries; serieNumber++)
                 {
                     Console.WriteLine("Serie number {0}", serieNumber + 1);
+                    player.play();
 
                     for (int repetitionNumber = 0; repetitionNumber < numOfReps; repetitionNumber++)
                     {
@@ -76,6 +93,7 @@
 
                     if (serieNumber < numOfSeries - 1)
                     {
+                        player.pause();
                         Console.WriteLine("Resting between series...");
                         Thread.Sleep(restTimeBetweenSeries * 1000);
                     }
@@ -83,6 +101,7 @@
 
                 if (exerciseNumber < numOfExercises - 1)
                 {
+                    player.pause();
                     Console.WriteLine("Resting between exercises...");
                     Thread.Sleep(restTimeBetweenExercises * 1000);
                 }
